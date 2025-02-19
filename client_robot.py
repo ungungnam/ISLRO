ad75a15187c3281adee40e20ead537e06d7c5112
ungungnam <unghui@DESKTOP-NE6S4BH>
2025-02-19 17:11:57 +0900

    added my name

--- client_robot.py
+++ client_robot.py
@@ -9,21 +9,40 @@ from constants import *
 
 server_ip = SERVER_IP
 
-async def send_data(uri, dataset_path):
-    async with websockets.connect(uri) as websocket:
-        # dataset = get_data(dataset_path)
-        dataset = "hello server"
+async def read_obs():
+    obs = read_camera()
+    return obs
 
-        await websocket.send(dataset)
-        response = await websocket.recv()
-        print(response)
+
+async def send_obs(obs, websocket):
+    await websocket.send(obs)
+    actions = await websocket.recv()
+
+    return actions
+
+
+async def actuate(actions):
+    command_robot(actions)
 
 
 async def main():
-    print("Sending data")
+    print("client opened")
     uri = f"ws://{server_ip}:8765"
-    dataset_path = "dataset"
 
-    await send_data(uri, dataset_path)
+    obs = None
+    actions = None
+
+    async with websockets.connect(uri) as websocket:
+        while True:
+            read_obs_task = asyncio.create_task(read_obs())
+            send_obs_task = asyncio.create_task(send_obs(obs, websocket))
+            actuate_task = asyncio.create_task(actuate(actions))
+
+            await read_obs_task
+            await send_obs_task
+            await actuate_task
+
+            obs = read_obs_task.result()
+            actions = send_obs_task.result()
 
 asyncio.run(main())
--- experiment_processed_data.py
+++ experiment_processed_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 from replay_episodes import EpisodeReplayer
 
 
@@ -9,7 +10,7 @@ def experiment(episode_name, control_mode, alt_control_mode, threshold):
     episode_replayer.alt_control_mode = alt_control_mode
 
     end_pose_data = episode_replayer.end_pose_data.copy()
-    episode_replayer.end_pose_data = process_data(end_pose_data)
+    episode_replayer.end_pose_data = process_data(end_pose_data, threshold)
 
     print(f'experiment {episode_name} {control_mode} {alt_control_mode} {threshold}')
     episode_replayer.replay()
@@ -19,11 +20,37 @@ def experiment(episode_name, control_mode, alt_control_mode, threshold):
 
 
 def process_data(data, threshold):
-    pass
+    x,y,z = data[:,0], data[:,1], data[:,2]
+    rx, ry, rz = data[:,3], data[:,4], data[:,5]
+
+    dx = np.diff(x)
+    dy = np.diff(y)
+    dz = np.diff(z)
+
+    dist = np.sqrt(np.power(dx,2) + np.power(dy,2) + np.power(dz,2))
+    indices = np.where(dist < threshold)[0]
+
+    x_re = np.hstack([x[0], x[indices]])
+    y_re = np.hstack([y[0], y[indices]])
+    z_re = np.hstack([z[0], z[indices]])
+
+    rx_re = np.stack([rx[0], rx[indices]])
+    ry_re = np.stack([ry[0], ry[indices]])
+    rz_re = np.stack([rz[0], rz[indices]])
+
+    processed_data = np.column_stack([x_re, y_re, z_re, rx_re, ry_re, rz_re])
+    return processed_data
 
 
 def main():
-    experiment()
+    episode_name = 'arrange_cups_2_30'
+    control_mode = 'EndPoseCtrl'
+
+    data1=experiment(episode_name, control_mode, None, 50)
+    data2=experiment(episode_name, control_mode, None, 100)
+    data3=experiment(episode_name, control_mode, None, 500)
+    data4=experiment(episode_name, control_mode, None, 1000)
+    data5=experiment(episode_name, control_mode, None, 5000)
 
 
 if __name__ == "__main__":
